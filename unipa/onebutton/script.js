7cee99c293faa75627d9e3f550dc7e85f799e14b
bellinux <bellinuxcom@gmail.com>
2022-10-12 10:39:13 +0200

    Add files via upload

--- unipa/onebutton/script.js
+++ unipa/onebutton/script.js
@@ -7,11 +7,65 @@ let speedFactor=6;
 
 let pressedThreshold=200;
 
+const now = Tone.now()
+const startValue=220;
+const osc = new Tone.Oscillator();//.toDestination();
+
+const sampler = new Tone.Sampler({
+			urls: {
+				A0: "A0.mp3",
+				C1: "C1.mp3",
+				"D#1": "Ds1.mp3",
+				"F#1": "Fs1.mp3",
+				A1: "A1.mp3",
+				C2: "C2.mp3",
+				"D#2": "Ds2.mp3",
+				"F#2": "Fs2.mp3",
+				A2: "A2.mp3",
+				C3: "C3.mp3",
+				"D#3": "Ds3.mp3",
+				"F#3": "Fs3.mp3",
+				A3: "A3.mp3",
+				C4: "C4.mp3",
+				"D#4": "Ds4.mp3",
+				"F#4": "Fs4.mp3",
+				A4: "A4.mp3",
+				C5: "C5.mp3",
+				"D#5": "Ds5.mp3",
+				"F#5": "Fs5.mp3",
+				A5: "A5.mp3",
+				C6: "C6.mp3",
+				"D#6": "Ds6.mp3",
+				"F#6": "Fs6.mp3",
+				A6: "A6.mp3",
+				C7: "C7.mp3",
+				"D#7": "Ds7.mp3",
+				"F#7": "Fs7.mp3",
+				A7: "A7.mp3",
+				C8: "C8.mp3"
+			},
+			release: 1,
+			onload: activateSound,
+			baseUrl: "https://tonejs.github.io/audio/salamander/"
+});
+
+soundRtmBool=false;
+function activateSound(){
+	soundRtmBool=true;
+}
+//osc.frequency.value = startValue;
+
+const panner = new Tone.Panner(0).toDestination();
+const pianoPanner = new Tone.Panner(-1).toDestination();
+var rtmDiff=600;
+
+//sampler.connect(pianoPanner).triggerAttackRelease([464], 0.1);
+var timeRtm=0;
 function step(timestamp) {
 	
+	//console.log(timestamp);
 	
-	
-	
+
 	if (speedFactor<diff){
 		speedFactor+=Math.abs((Math.abs(speedFactor)-Math.abs(diff))/500);
 	} else {
@@ -22,11 +76,29 @@ function step(timestamp) {
 	let pLeft=(parseFloat(compStyles.getPropertyValue('left')) + (xMov/speedFactor));
 	let pTop=(parseFloat(compStyles.getPropertyValue('top')) + (yMov/speedFactor));
 	
-	if (pLeft<0) {pLeft=0;  /* xMov=xMov*-1; increment+=3.14; */  };
-	if (pTop<0) {pTop=0; /* yMov=yMov*-1; increment+=3.14; */ };
+	if (pLeft<10) {pLeft=10;  /* xMov=xMov*-1; increment+=3.14; */  };
+	if (pTop<10) {pTop=10; /* yMov=yMov*-1; increment+=3.14; */ };
 	
-	if (pLeft>window.innerWidth-10) { pLeft=window.innerWidth-10; /*  xMov=xMov*-1; increment+=3.14; */ };
-	if (pTop>window.innerHeight-10) { pTop=window.innerHeight-10; /*  yMov=yMov*-1; increment+=3.14; */ };
+	if (pLeft>window.innerWidth-30) { pLeft=window.innerWidth-30; /*  xMov=xMov*-1; increment+=3.14; */ };
+	if (pTop>window.innerHeight-30) { pTop=window.innerHeight-30; /*  yMov=yMov*-1; increment+=3.14; */ };
+	
+	
+	if ((timestamp-timeRtm)>rtmDiff && soundRtmBool) {
+		
+		rtmPanner=((2/window.innerWidth)*pLeft)-1;
+		//console.log(rtmPanner);
+		
+		rtmTone=Math.abs(pTop-window.innerHeight)+200;
+		//console.log(rtmTone);
+		
+		pianoPanner.pan.rampTo(rtmPanner, now);
+		sampler.connect(pianoPanner).triggerAttackRelease([rtmTone], 0.1);
+		
+		//console.log(rtmDiff);
+		timeRtm=timestamp;
+	}
+	
+	
 	
 	
 	
@@ -46,9 +118,18 @@ function step(timestamp) {
 		xMov=Math.sin(increment);
 		yMov=Math.cos(increment);
 		var angle=parseInt((Math.atan2(yMov, xMov) * (180/Math.PI))) - 135;
-		//console.log(angle);
+		
 		
 		dot.style.transform = "rotate("+angle+"deg)";
+		
+		var sonicAngle=Math.abs(parseInt((Math.atan2(xMov, yMov) * (180/Math.PI))));
+		var sonicPanner=parseFloat((Math.atan(xMov, yMov) * (180/Math.PI)))/45;
+		//console.log(sonicPanner);
+		
+		//if (sonicPanner>0) { sonicPanner=1; } else {sonicPanner=-1;}
+		
+		panner.pan.rampTo(sonicPanner, now);
+		osc.connect(panner).frequency.rampTo(parseInt(startValue+sonicAngle), now);
 		
 	}
 	window.requestAnimationFrame(step);
@@ -78,6 +159,7 @@ function triggerDown(e){
 	  rotating=true;
 	  oldDiff=diff;
 	  oldSpeedFactor=speedFactor;
+	  osc.start();
 	  //console.log(oldDiff);
 	}, pressedThreshold)
 }
@@ -90,7 +172,9 @@ function triggerUp(e){
 		intervals.shift();
 		if (intervals[0] != 0){
 			//console.log(intervals);
-			diff = ((intervals[1]-intervals[0]) / 1000);
+			
+			rtmDiff = (intervals[1]-intervals[0]);
+			diff = rtmDiff/1000;
 			//console.log(speedFactor);
 
 		}
@@ -103,6 +187,7 @@ function triggerUp(e){
 	
 	allowed = true;
 	rotating = false;
+	osc.stop();
 }
 
 document.addEventListener("keydown", triggerDown);
