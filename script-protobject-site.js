f4bbbe8f34b6349cad4e99a619d02d1b190cba3f
bellinux <bellinuxcom@gmail.com>
2023-02-04 22:45:33 -0300

    Update script-protobject-site.js

--- script-protobject-site.js
+++ script-protobject-site.js
@@ -1,29 +1,33 @@
-$(document).on( "click", "#class-plan", function() {
-	var element = $("#classtext").find('.et_pb_toggle_content')[0];
 
-	var newWin = open('url', '_blank');
-	var html = `
 
-	<html>
-	<head>  
-	<title>Plan de clase</title>
-	<style>
-	html{color: #000!important; font-family: sans-serif;}
+$( document ).ready(function() {
+	$(document).on( "click", "#class-plan", function() {
+		var element = $("#classtext").find('.et_pb_toggle_content')[0];
 
-	</style>
-	</head>
-	<body>${element.innerHTML}</body>
+		var newWin = open('url', '_blank');
+		var html = `
 
-	<script>
-	var elements = document.querySelectorAll("*");
+		<html>
+		<head>  
+		<title>Plan de clase</title>
+		<style>
+		html{color: #000!important; font-family: sans-serif;}
 
-	elements.forEach((note) => {
-		note.style.color = '#000';
+		</style>
+		</head>
+		<body>${element.innerHTML}</body>
+
+		<script>
+		var elements = document.querySelectorAll("*");
+
+		elements.forEach((note) => {
+			note.style.color = '#000';
+		});
+		window.print();
+		</script>
+		</html>;
+
+		`;
+		newWin.document.write(html);
 	});
-	window.print();
-	</script>
-	</html>;
-
-	`;
-	newWin.document.write(html);
 });
