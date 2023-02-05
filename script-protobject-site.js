90e0dace5f9b0da7c2aaaac1d1de4ec3d055ff6c
bellinux <bellinuxcom@gmail.com>
2023-02-04 22:36:51 -0300

    Create script-protobject-site.js

---
+++ script-protobject-site.js
@@ -0,0 +1,29 @@
+$( "#class-plan" ).click(function() {
+	var element = $("#classtext").find('.et_pb_toggle_content')[0];
+
+	var newWin = open('url', '_blank');
+	var html = `
+
+	<html>
+	<head>  
+	<title>Plan de clase</title>
+	<style>
+	html{color: #000!important; font-family: sans-serif;}
+
+	</style>
+	</head>
+	<body>${element.innerHTML}</body>
+
+	<script>
+	var elements = document.querySelectorAll("*");
+
+	elements.forEach((note) => {
+		note.style.color = '#000';
+	});
+	window.print();
+	</script>
+	</html>;
+
+	`;
+	newWin.document.write(html);
+});
