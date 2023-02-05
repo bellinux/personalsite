03e9916ee0b2c64cc99987274172ea0a4b09c9ae
bellinux <bellinuxcom@gmail.com>
2023-02-05 12:13:00 -0300

    Update script-protobject-site.js

--- script-protobject-site.js
+++ script-protobject-site.js
@@ -1,6 +1,29 @@
 $( document ).ready(function() {
 
-	
+$(document).on( "click", "#class-plan", function() {
+		var element = $("#classtext").find('.et_pb_toggle_content')[0];
+
+		var newWin = open('url', '_blank');
+		var html = `
+		<html>
+		<head>  
+		<title>Plan de clase</title>
+		<style>
+		html{color: #000!important; font-family: sans-serif;}
+		</style>
+		</head>
+		<body>${element.innerHTML}</body>
+		<script>
+		var elements = document.querySelectorAll("*");
+		elements.forEach((note) => {
+			note.style.color = '#000';
+		});
+		window.print();
+		</script>
+		</html>;
+		`;
+		newWin.document.write(html);
+	});
 	
 	
 
